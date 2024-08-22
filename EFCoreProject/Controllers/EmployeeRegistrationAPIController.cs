1fb0fa7e3d6157d0917eda37234394fc80ca09ab
amol404 <pamol3905@gmail.com>
2024-08-22 17:22:31 +0530

    New APIs are created

--- EFCoreProject/Controllers/EmployeeRegistrationAPIController.cs
+++ EFCoreProject/Controllers/EmployeeRegistrationAPIController.cs
@@ -20,7 +20,22 @@ namespace JobPortalApp.Controllers
         [HttpGet]
         public async Task<ActionResult<Employee>> GetAllEmployee()
         {
+            // Getting all columns from table
             var result = await (from Employee in _appDbContext.Employees select Employee).ToListAsync();
+
+            // Getting selected columns from table (other Class props values are null is this case)
+            //var result = await (from Employee in _appDbContext.Employees select new Employee { Id = Employee.Id, Name = Employee.Name}).ToListAsync();
+
+            // Getting selected columns from table (Other class props are not shown in object)
+            //var result = await (from Employee in _appDbContext.Employees select new { Id = Employee.Id, Name = Employee.Name }).ToListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeByID([FromRoute] int id)
+        {
+            var result = await _appDbContext.Employees.FindAsync(id);
 
             return Ok(result);
         }
